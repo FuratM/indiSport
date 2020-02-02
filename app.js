7fe110575cbdd2836b630bc1665a4c9a75fab742
furat <furat@gmail.com>
2020-02-02 18:39:13 +0100

    removed controller, and made code visible in route instead

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const fs = require('fs');
 const port = 3010;
 
 //----------------- CONTROLLERS --------------------------
-const welcomeController = require('./controllers/welcome');
+//const welcomeController = require('./controllers/welcome');
 
 
 //------------------- DB CONNECTION -------------------
@@ -65,7 +65,21 @@ app.use(function(req, res, next){
 
 //move this to users-folder.. remember try: /users/welcome
 app.get('/welcome', function(req, res, next){
-  welcomeController.goToWelcome(req, res);
+  //function goToWelcome(req, res) {  //IMPORTANT: A GET AND NOT POST REQUEST BECAUSE WE ARE NOT RECIEVING DATA THROUGH FORM...
+
+    if (req.session.email_login) { // req.cookies.email_login / req.session.email_login
+      console.log('---------------------->', `uploads/${req.session.p_img}`)
+      res.render('welcome', { // req.body.username wont work, we try to get some cookies, not data...
+         email: req.session.email_login, //name: username, value: req.cookies.username
+         fname: req.session.fname,
+         art: req.session.art,
+         p_img: req.session.p_img, // profile image to be sent into the view...
+         file: `uploads/${req.session.p_img}`
+      });
+    }else{
+      res.redirect('/users/login?msg=noLogin');
+    }
+  // }
 });
 
 //--------------------FILE UPLOAD----------------------------
--- controllers/welcome.js
+++ controllers/welcome.js
@@ -1,19 +1,19 @@
-function goToWelcome(req, res) {  //IMPORTANT: A GET AND NOT POST REQUEST BECAUSE WE ARE NOT RECIEVING DATA THROUGH FORM...
-
-  if (req.session.email_login) { // req.cookies.email_login / req.session.email_login
-    console.log('---------------------->', `uploads/${req.session.p_img}`)
-    res.render('welcome', { // req.body.username wont work, we try to get some cookies, not data...
-       email: req.session.email_login, //name: username, value: req.cookies.username
-       fname: req.session.fname,
-       art: req.session.art,
-       p_img: req.session.p_img, // profile image to be sent into the view...
-       file: `uploads/${req.session.p_img}`
-    });
-  }else{
-    res.redirect('/users/login?msg=noLogin');
-  }
-}
-
-module.exports = {
-  goToWelcome: goToWelcome
-}
+// function goToWelcome(req, res) {  //IMPORTANT: A GET AND NOT POST REQUEST BECAUSE WE ARE NOT RECIEVING DATA THROUGH FORM...
+//
+//   if (req.session.email_login) { // req.cookies.email_login / req.session.email_login
+//     console.log('---------------------->', `uploads/${req.session.p_img}`)
+//     res.render('welcome', { // req.body.username wont work, we try to get some cookies, not data...
+//        email: req.session.email_login, //name: username, value: req.cookies.username
+//        fname: req.session.fname,
+//        art: req.session.art,
+//        p_img: req.session.p_img, // profile image to be sent into the view...
+//        file: `uploads/${req.session.p_img}`
+//     });
+//   }else{
+//     res.redirect('/users/login?msg=noLogin');
+//   }
+// }
+//
+// module.exports = {
+//   goToWelcome: goToWelcome
+// }
